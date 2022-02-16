ea8e2763af5da0d99286ad8716bb82506a381d1e
VincentDinh2305 <90953477+VincentDinh2305@users.noreply.github.com>
2022-02-16 06:00:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_RationalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+            Rational r1 = new Rational();
+            Rational r2 = new Rational(denominator: 5);
+            Rational r3 = new Rational(7, 4);
+            Rational r4 = new Rational(8, 5);
+
+            //#2
+            Console.WriteLine($"Rational 01: {r1.ToString()}");
+            Console.WriteLine($"Rational 01: {r2.ToString()}");
+            r1.IncreaseBy(r2);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Rational 01: {r1.ToString()}");
+            Console.WriteLine($"Rational 01: {r2.ToString()}");
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("---------------------------");
+
+            //#3
+            Console.WriteLine($"Rational 03: {r3.ToString()}");
+            Console.WriteLine($"Rational 04: {r4.ToString()}");
+            r3.DecreaseBy(r4);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Rational 03: {r1.ToString()}");
+            Console.WriteLine($"Rational 04: {r2.ToString()}");
+        }
+    }
+}
---
+++ Rational.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_RationalClass
+{
+    class Rational
+    {
+        public int Denominator { get; private set; }
+        public int Numerator { get; private set; }
+
+        public Rational(int numerator = 0, int denominator = 1)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public void IncreaseBy(Rational other)
+        {
+            if (Denominator == other.Denominator)
+            {
+                Numerator += other.Numerator;
+            }
+            else
+            {
+                Numerator = Numerator * other.Denominator + Denominator * other.Numerator;
+                Denominator = Denominator * other.Denominator;
+            }
+            //Numerator = Numerator * other.Denominator + Denominator * other.Numerator;
+            //Denominator = Denominator * other.Denominator;
+        }
+
+        public void DecreaseBy(Rational other)
+        {
+            if (Denominator == other.Denominator)
+            {
+                Numerator += other.Numerator;
+            }
+            else
+            {
+                Numerator = Numerator * other.Denominator - Denominator * other.Numerator;
+                Denominator = Denominator * other.Denominator;
+            }
+            //Numerator = Numerator * other.Denominator - Denominator * other.Numerator;
+            //Denominator = Denominator * other.Denominator;
+        }
+    }
+}
